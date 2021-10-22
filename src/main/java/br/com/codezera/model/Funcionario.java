322d9fcff89519647b33089b02938cf5e33242dd
copquesz <copque.faculdade@gmail.com>
2021-10-22 19:42:33 -0300

    adiciona model funcionario

---
+++ src/main/java/br/com/codezera/model/Funcionario.java
@@ -0,0 +1,32 @@
+package br.com.codezera.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@DynamicUpdate
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "funcionario")
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private Date profissao;
+
+    private Double salario;
+}
