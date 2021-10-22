9473086c2ba5c1c4eca47f2f52b13816b63ac990
copquesz <copque.faculdade@gmail.com>
2021-10-22 19:27:57 -0300

    inicia projeto

---
+++ src/main/java/br/com/codezera/SampleRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.codezera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleRestSpringBootApplication.class, args);
+    }
+
+}
