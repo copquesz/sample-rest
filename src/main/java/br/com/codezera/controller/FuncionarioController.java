9e839e6f6c47a8b15c7d84b5cceba37a482c0bc1
copquesz <copque.faculdade@gmail.com>
2021-10-22 21:14:16 -0300

    adiciona endpoints dos verbos hhttp restantes e refatora as urls

--- src/main/java/br/com/codezera/controller/FuncionarioController.java
+++ src/main/java/br/com/codezera/controller/FuncionarioController.java
@@ -6,23 +6,78 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Log
+@RequestMapping("funcionarios")
 public class FuncionarioController {
 
     private final FuncionarioRepository funcionarioRepository;
 
-    @PostMapping("funcionarios/salvar")
+    @PostMapping
     public ResponseEntity<Funcionario> save(@RequestBody Funcionario funcionario) {
         funcionario = this.funcionarioRepository.save(funcionario);
         log.info("Usuário criado com sucesso :: ID " + funcionario.getId());
         return new ResponseEntity<>(funcionario, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> update(@PathVariable Long id, @RequestBody Funcionario funcionarioUpdated) {
+        Optional<Funcionario> optionalFuncionario = this.funcionarioRepository.findById(id);
+        if (optionalFuncionario.isPresent()) {
+            Funcionario funcionario = optionalFuncionario.get();
+            funcionario.setNome(funcionarioUpdated.getNome());
+            funcionario.setProfissao(funcionario.getProfissao());
+            funcionario.setSalario(funcionarioUpdated.getSalario());
+            this.funcionarioRepository.save(funcionario);
+            log.info("Usuário atualizado com sucesso :: ID " + funcionario.getId());
+            return new ResponseEntity<>(funcionario, HttpStatus.CREATED);
+        } else {
+            log.info("Usuário não encontrado :: ID " + id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionario> findById(@PathVariable Long id) {
+        Optional<Funcionario> funcionario = this.funcionarioRepository.findById(id);
+        if (funcionario.isPresent()) {
+            log.info("Buscando usuário :: ID " + id);
+            return new ResponseEntity<>(funcionario.get(), HttpStatus.OK);
+        } else {
+            log.info("Usuário não encontrado :: ID " + id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> findAll() {
+        List<Funcionario> funcionarios = this.funcionarioRepository.findAll();
+        if (!funcionarios.isEmpty()) {
+            log.info("Listando usuários");
+            return new ResponseEntity<>(funcionarios, HttpStatus.OK);
+        } else {
+            log.info("Não há usuários cadastrados");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Funcionario> deleteById(@PathVariable Long id) {
+        Optional<Funcionario> funcionario = this.funcionarioRepository.findById(id);
+        if (funcionario.isPresent()) {
+            log.info("Deleta usuário :: ID " + id);
+            this.funcionarioRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            log.info("Usuário não encontrado :: ID " + id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
