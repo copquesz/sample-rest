f9dea9671944352170a4b4ba0779d3845b5e8ff3
Matheus Henrique Ramires <math.h.ramires@gmail.com>
2021-11-18 17:09:02 -0300

    FeatSecurity
    Adiciona passwordEnconder e a classe security para autenticação

--- src/main/java/br/com/codezera/config/SecurityConfig.java
+++ src/main/java/br/com/codezera/config/SecurityConfig.java
@@ -1,7 +1,35 @@
 package br.com.codezera.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity
-public class SecurityConfig {
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String senhaCriptografada = encoder.encode("123");
+        System.out.println(senhaCriptografada);
+        auth.inMemoryAuthentication()
+                .withUser("matheus").password(senhaCriptografada).roles("USER","ADMIN");
+
+    }
 }
